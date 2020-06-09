1de609985f3d8edd1dff9b3f474659a662c63e99
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-09 22:44:17 +1000

    tested our model and it is working in rails console

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+
+    def create
+        @task = Task.new(task_params)
+    end
+
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details)
+    end
 end

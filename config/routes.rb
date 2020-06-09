b81ffe1d13281e69716213e4c86cc537e5ad34bd
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-09 22:33:14 +1000

    generated our routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'task#index'
+  get '/tasks/:id', to: 'task#show'
+  get '/tasks/new', to: 'task#new'
+  post '/tasks', to: 'task#create'
+  get '/tasks/:id/edit', to: 'task#edit'
+  patch '/tasks/:id', to:  'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
